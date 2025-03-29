d7a4cb3cae0629cd31fc112dd7b6a4f23dfd30a7
Thesupernile <62834678+Thesupernile@users.noreply.github.com>
2025-03-29 14:48:42 +0000

    Update BlackJack.py

--- BlackJack.py
+++ BlackJack.py
@@ -1,5 +1,5 @@
 #############################################################################
-# Copyright © 2024 Daniel Smith                                             #               
+# Copyright © 2024 Thesupernile                                             #               
 #                                                                           #
 #                                                                           #
 # Licensed under the Apache License, Version 2.0 (the "License");           #
